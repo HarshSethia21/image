f5296c0394d0413eef6dc080043766d21841fe95
HarshSethia21 <87221233+HarshSethia21@users.noreply.github.com>
2021-10-30 11:02:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function turnon(){
+document.getElementById("bulb").src="https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg"
+}
+function turnoff(){
+    document.getElementById("bulb").src="https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg"
+    }
